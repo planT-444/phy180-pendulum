adbfc47e12a5d8628b6f091dac4bea88ce2d3e8d
planT-444 <williamhu03@gmail.com>
2025-11-09 18:03:54 -0500

    trying to parse the files :,)

--- yippee2.py
+++ yippee2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 This program will find the best fit of a given function to a given set
 of data (including errorbars). It prints the results, with uncertainties.
@@ -23,112 +22,69 @@ import matplotlib.pyplot as plt
 import statistics
 from pathlib import Path
 
-def load_q(filename):
+def load_fit_exponential_amplitudes(lengths):
     output = ""
     input_dir = Path(__file__).parent / "input-files"
+    period_filename = input_dir / "PHY180 Pendulum - T vs L.csv"
 
-    filename = input_dir / filename
-    lengths, Qs, Q_errors = np.genfromtxt(filename, usecols=(0,3,4), skip_header=1, unpack = True, delimiter = ',')
-    length_errors = np.array([0.5] * len(lengths))
+    data = np.genfromtxt(period_filename, usecols=(0,1), skip_header=1, delimiter = ',')
+    filtered_periods = {}
+    
+    for length, period in data:
+        if length not in filtered_periods:
+            filtered_periods[length] = []
+        filtered_periods[length].append(period)
+        
+    lengths = []
+    mean_periods = []
+    period_error = []
+    period_b_uncertainty = 0.0008 # 120 fps, 10 oscillations
+    for length, periods in filtered_periods.items():
+        lengths.append(length)
+        mean_periods.append(np.mean(periods))
+        period_error.append(max(
+            period_b_uncertainty, 
+            statistics.stdev(periods) / len(periods) ** 0.5
+        ))
     print(lengths)
-    print(Qs)
-            
-    return lengths, Qs, length_errors, Q_errors
+    print(mean_periods)
 
-def plot_fit(my_func, xdata, ydata, xerror=None, yerror=None, init_guess=None, font_size=30,
-             xlabel="", ylabel="Period (s)", 
-             title="",
-             output_filename = "graph.png"
-             ):    
-    plt.rcParams.update({'font.size': font_size})
-    plt.rcParams['figure.figsize'] = 18, 9
-    # Change the fontsize of the graphs to make it easier to read.
-    # Also change the picture size, useful for the save-to-file option.
+    lengths, mean_periods, period_error = zip(
+        *sorted(
+            zip(lengths, mean_periods, period_error)
+        )
+    )
+    print(lengths)
+    print(mean_periods)
+    print('\n\n\n')
+    
+    for i, length in enumerate(sorted(lengths)):
+        filename = input_dir / f"PHY180 Pendulum - L={length}.csv"
+        times, amplitudes = np.genfromtxt(filename, usecols=(0,1), skip_header=1, unpack = True, delimiter = ',')
+        time_error = np.array([1/120] * len(times))
+        amplitude_error = np.array([np.deg2rad(0.3)] * len(amplitudes))
+        print(times)
+        print(amplitudes)
                
-    popt, pcov = optimize.curve_fit(my_func, xdata, ydata, sigma=yerror, p0=init_guess, absolute_sigma=True)
-    # The best fit values are popt[], while pcov[] tells us the uncertainties.
+        popt, pcov = optimize.curve_fit(exponential, times, amplitudes, sigma=amplitude_error, p0=None, absolute_sigma=True)
+        # The best fit values are popt[], while pcov[] tells us the uncertainties.
 
-    puncert = np.sqrt(np.diagonal(pcov))
-    # The uncertainties are the square roots of the diagonal of the covariance matrix
-    
-    start = min(xdata)
-    stop = max(xdata)    
-    xs = np.arange(start,stop,(stop-start)/1000) 
-    curve = my_func(xs, *popt) 
-    # (x,y) = (xs,curve) is the line of best fit for the data in (xdata,ydata).
-    # It has 1000 points to make it look smooth.
-    # Note: the "*" tells Python to send all the popt values in a readable way.
-    
-    fig, (ax1,ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]})
-    # Make 2 graphs above/below each other: ax1 is top, ax2 is bottom.
-    # The gridspec_kw argument makes the top plot 2 times taller than the bottom plot.
-    # You can adjust the relative heights by, say, changing [2, 1] to [3, 1].
-    
-    ax1.errorbar(xdata, ydata, yerr=yerror, xerr=xerror, fmt=".", label="data", color="black")
-    # Plot the data with error bars, fmt makes it data points not a line, label is
-    # a string which will be printed in the legend, you should edit this string.
-
-    ax1.plot(xs, curve, label="best fit", color="black")
-    # Plot the best fit curve on top of the data points as a line.
-    # NOTE: you may want to change the value of label to something better!!
-
-    ax1.legend(loc='upper right' if output_filename=="amplitude vs. time" else 'lower right')
-    # Prints a box using what's in the "label" strings in the previous two lines.
-    # loc specifies the location
-
-    ax1.set_xlabel(xlabel)
-    ax1.set_ylabel(ylabel)
-    ax1.set_title(title)
-    # label the axes and set a title
-
-    #ax1.set_xscale('log')
-    #ax1.set_yscale('log')
-    # uncomment out the above two lines if you want to make it log-log scale
-    
-    residual = ydata - my_func(xdata, *popt)
-    ax2.errorbar(xdata, residual, yerr=yerror, xerr=xerror, fmt=".", color="black")
-    # Plot the residuals with error bars.
-    
-    ax2.axhline(y=0, color="black")    
-    # Plot the y=0 line for context.
-    
-    ax2.set_xlabel(xlabel)
-    ax2.set_ylabel("Residuals")
-    # ax2.set_title("Residuals of the fit")
-    # Here is where you change how your graph is labelled.
-
-    fig.tight_layout()
-    # Does a decent cropping job of the two figures.
-    
-    output_dir = Path(__file__).parent / "output-files"
-    
-    with open(output_dir / (output_filename + ".txt"), 'w') as f:
-        f.write("Best fit parameters, with uncertainties, but not rounded off properly:\n")
+        puncert = np.sqrt(np.diagonal(pcov))
+        # The uncertainties are the square roots of the diagonal of the covariance matrix
+        output += f"Length {length}:\n"
         for i in range(len(popt)):
-            f.write(f"{popt[i]} +/- {puncert[i]}\n")
-    
-    fig.savefig(output_dir / (output_filename + ".png"))
-    # Show the graph on your screen.
+            output += f"{popt[i]} +/- {puncert[i]}\n"
+        output += "\n"
+        
+    output_dir = Path(__file__).parent / "output-files"
+        
+    with open(output_dir / ("lengths and taus.txt"), 'w') as f:
+        f.write("Best fit parameters, with uncertainties, but not rounded off properly:\n")
+        f.write(output)
 
-    
-    # This saves the graph as a file, which will get overwritten
-    # every time you run this program, so rename the file if you
-    # want to keep multiple files!
-
-
-
-def quadratic(L, A, B, C):
-    return A * L ** 2 + B * L + C
+def exponential(time, theta_0, tau):
+    return theta_0 * np.e ** -(time/tau)
 
 
 if __name__ == '__main__':
-    plot_fit(quadratic, 
-             *load_q("PHY180 Pendulum - Q vs L.csv"),
-             xlabel="Length (cm)",
-             ylabel="Q (s)",
-             output_filename = "Q vs length")
-    plot_fit(quadratic, 
-             *load_q("PHY180 Pendulum - GET OUT.csv"),
-             xlabel="Length (cm)",
-             ylabel="Q (s)",
-             output_filename = "Q vs length (no outlier)")
+    load_fit_exponential_amplitudes([36.5])
